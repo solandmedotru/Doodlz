afc463ac64954c22eaeae6330f7d4c757d810aac
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-17 10:41:04 +0300

    added DoodleView class

---
+++ app/src/main/java/DoodleView.java
@@ -0,0 +1,9 @@
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoodleView extends View {
+    public DoodleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
